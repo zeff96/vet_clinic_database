aeb1412b19c22144ef67ae71dac322cf998ebfdb
zeff96 <zeffo1975@gmail.com>
2023-05-01 18:01:09 +0300

    Add query for all date of birth for animals with names either pikachu or agumon.

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 SELECT * FROM animals WHERE name LIKE '%mon%';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered = 't' AND escape_attempts < 3;
+SELECT date_of_birth FROM animals  WHERE name = 'Agumon' OR name = 'Pikachu';
