759f5755cd59192be4a56556a4745b4dbb3b7e64
zeff96 <zeffo1975@gmail.com>
2023-05-01 17:08:09 +0300

    Add the first animal data.

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Agumon', TO_DATE('Feb 3rd,2020', 'Mon DDth, YYYY'), 0, true, 10.23);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
