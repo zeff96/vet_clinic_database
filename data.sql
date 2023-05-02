96650c743debf42bfbccc496475be9c015d92e34
zeff96 <zeffo1975@gmail.com>
2023-05-02 12:36:13 +0300

    Add animal data for Plantom to the table

--- data.sql
+++ data.sql
@@ -5,3 +5,4 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) V
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', TO_DATE('Jan 7th,2021', 'Mon DDth YYYY'), 1, false, 15.04);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon', TO_DATE('May 12th,2017', 'Mon DDth YYYY'), 5, true, 11);
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Charmander', TO_DATE('Feb 8th,2020','Mon DDth YYYY'), 0, false, 11);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)  VALUES('Plantom', TO_DATE('Nov 15th,2021','Mon DDth YYYY'), 2, true, 5.7);
